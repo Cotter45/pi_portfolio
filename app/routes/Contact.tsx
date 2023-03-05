587899e498f823b09b6e3f6a2fd0b1d4dccaecc3
Sean <sd.cotter45@gmail.com>
2023-03-05 11:10:33 -0500

    changed theme colors on form

--- app/routes/Contact.tsx
+++ app/routes/Contact.tsx
@@ -7,7 +7,7 @@ export default function Contact() {
   return (
     <PageContainer>
       <div className="mt-10 py-8 lg:py-16 px-4 mx-auto w-full max-w-screen-md">
-        <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">
+        <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-white">
           Contact Me
         </h2>
         <div className="flex items-center justify-center space-x-4 mt-4 mx:text=xl">
@@ -27,7 +27,7 @@ export default function Contact() {
               type="email"
               id="email"
               name="email"
-              className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+              className="shadow-sm border text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-primary-500 focus:border-primary-500 shadow-sm-light"
               placeholder="name@place.com"
               required
             />
@@ -43,7 +43,7 @@ export default function Contact() {
               type="text"
               id="subject"
               name="subject"
-              className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+              className="block p-3 w-full text-sm rounded-lg border shadow-sm focus:ring-primary-500 focus:border-primary-500 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-primary-500 focus:border-primary-500 shadow-sm-light"
               placeholder="What's this about?"
               required
             />
@@ -59,7 +59,7 @@ export default function Contact() {
               id="html"
               name="html"
               rows={6}
-              className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 whitespace-pre-wrap"
+              className="block p-2.5 w-full text-sm rounded-lg shadow-sm border focus:ring-primary-500 focus:border-primary-500 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-primary-500 focus:border-primary-500 whitespace-pre-wrap"
               placeholder="Leave a comment..."
             ></textarea>
           </div>
